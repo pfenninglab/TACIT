c85de6866717332dc68f6c9a479a7b7ab813eb37
Daniel Schaffer <55718373+schafferde@users.noreply.github.com>
2022-11-21 15:56:52 -0500

    Update pdists.R

--- evaluationScriptsEnhancerPhenotypeAssociations/pdists.R
+++ evaluationScriptsEnhancerPhenotypeAssociations/pdists.R
@@ -1,14 +1,18 @@
 
 pvals = read.csv("brainsize_pdists.csv")
 
-svg(file = "cortex_p.svg", width=5, height=5)
-d = density(pvals$Cortex_Perm_Pvalue)
-plot(d, xlab = "Permulations p-values", ylab="Density", main="",ylim=c(0,2))
-polygon(d, col="grey", border="black", lwd=2)
+svg(file = "cortex_staggered_p.svg", width=4.5, height=5)
+hist(na.omit(pvals$Cortex_BrainSize_Staggered), xlab = "Brain Size Staggered Perumlations p-value", ylab="# of Cortex OCRs", main="")
 dev.off()
 
-svg(file = "pv_p.svg", width=5, height=5)
-d = density(na.omit(pvals$PV_Perm_Pvalue))
-plot(d, xlab = "Permulations p-values", ylab="Density", main="", ylim=c(0,2))
-polygon(d, col="grey", border="black", lwd=2)
+svg(file = "cortex_10k_p.svg", width=4.5, height=5)
+hist(na.omit(pvals$Cortex_BrainSize_10K), xlab = "Brain Size 10K Perumlations p-value", ylab="# of Cortex OCRs", main="")
+dev.off()
+
+svg(file = "pv_staggered_p.svg", width=4.5, height=5)
+hist(na.omit(pvals$PV_BrainSize_Staggered), xlab = "Brain Size Staggered Perumlations p-value", ylab="# of PV Neuron OCRs", main="")
+dev.off()
+
+svg(file = "pv_10k_sol_p.svg", width=4.5, height=5)
+hist(na.omit(pvals$PV_Solitary_10K), xlab = "Solitary 10K Perumlations p-value", ylab="# of PV Neuron OCRs", main="", xlim=c(0,1))
 dev.off()
