f254e8eb174741191713f5a1f604a9426c438d22
Daniel Schaffer <55718373+schafferde@users.noreply.github.com>
2023-01-04 17:32:26 -0500

    Add variants of distribution-shift scripts

---
+++ evaluationScriptsEnhancerPhenotypeAssociations/p_dist_collect_tab.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+#python3 p_dist_.py gene-list gene-peak-file p-val-file outFile
+
+
+import sys
+from bisect import insort
+from scipy.stats import ranksums
+
+
+geneFile = open(sys.argv[1], "r")
+geneSet = set([x.strip() for x in geneFile])
+geneFile.close()
+
+peaksFile = open(sys.argv[2], "r")
+fgPeaks = set()
+allPeaks = set()
+for line in peaksFile:
+    tokens = line.strip().split()
+    peak = tokens[3]
+    gene = tokens[7]
+    allPeaks.add(peak)
+    if gene in geneSet:
+        fgPeaks.add(peak)
+peaksFile.close()
+
+bgPeaks = allPeaks - fgPeaks
+
+fgPvals = []
+bgPvals = []
+pValFile = open(sys.argv[3], "r")
+pValFile.readline()
+for line in pValFile:
+    tokens = line.strip().split()
+    p = float(tokens[3])
+    name = tokens[0].replace("\"", "")
+    if name in fgPeaks:
+        insort(fgPvals, p)
+    elif name in bgPeaks:
+        insort(bgPvals, p)
+pValFile.close()
+
+print(len(fgPvals), len(bgPvals))
+
+print(ranksums(fgPvals, bgPvals, "less"))
+
+outFile = open(sys.argv[4], "w")
+outFile.write("pval,fg\n")
+for pval in fgPvals:
+    outFile.write(str(pval) + ",T\n")
+for pval in bgPvals:
+    outFile.write(str(pval) + ",F\n")
+outFile.close()
+
