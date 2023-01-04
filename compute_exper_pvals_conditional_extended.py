63b9bf68d83c98f7a73c21ee2bc7ea3612e631ca
Daniel Schaffer <55718373+schafferde@users.noreply.github.com>
2023-01-04 17:10:36 -0500

    Upload conditional scripts

---
+++ compute_exper_pvals_conditional_extended.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+#foo.py [file with p-vals] [number of folders] [folders with random results] [output file] [max # of trials to consume] [file for extra trials]
+import os, sys
+from math import log10, ceil
+
+def parseNum(s):
+    te = s.split("e")
+    if len(te) == 2:
+        num = float(te[0])
+        base = int(te[1])
+    else:
+        num = float(s)
+        base = 0
+    return [num, base]
+
+def leq(a, b):
+    randNum, randBase = a
+    realNum, realBase = b
+    return randBase < realBase or (realBase == randBase and randNum <= realNum)
+
+
+inFile = open(sys.argv[1], "r")
+inFile.readline() 
+header = '"Enhancer","Pvalue","Coeff"\n'
+new_header = '"Enhancer","Pvalue","Coeff","Exp_Pvalue","Trials","Missing_Trials","Low_Trials"\n'
+names = []
+pvals = []
+parsed_pvals = {}
+#corPvals = []
+coeffs = []
+coeffs_negative = {}
+lower_trials = {}
+count_trials = {}
+for line in inFile:
+    tokens = line.strip().replace("\"", "").split(",")
+    if len(tokens) >= 2:
+        names.append(tokens[0])
+        pvals.append(tokens[1])
+        parsed_pvals[tokens[0]] = parseNum(tokens[1])
+        #corPvals.append(tokens[2])
+        coeffs.append(tokens[2])
+        coeffs_negative[tokens[0]] = tokens[2][0] == "-"
+        if len(tokens) >= 6:
+            lower_trials[tokens[0]] = int(tokens[6])
+            count_trials[tokens[0]] = int(tokens[4])
+        else:
+            lower_trials[tokens[0]] = 1
+            count_trials[tokens[0]] = 1
+            
+inFile.close()
+
+#lower_trials = {}
+#count_trials = {}
+#for name in names:
+#    lower_trials[name] = 1
+#    count_trials[name] = 1
+
+nDir = int(sys.argv[2])
+
+max_trials = int(sys.argv[4+nDir])
+extraFile = open(sys.argv[5+nDir], "w")
+extraFile.write(header)
+ 
+for i in range(nDir):   
+    inDir = sys.argv[3+i]
+    print(inDir)
+    for f in os.listdir(inDir):
+        inFile = open(inDir + "/" + f, "r") 
+        header = inFile.readline().strip().replace("\"", "").split(",")
+        try:
+            column = header.index("Pvalue")
+            column2 = header.index("Coeff")
+        except:
+            print(f, file=sys.stderr)
+            continue
+        for line in inFile:
+            tokens = line.strip().replace("\"", "").split(",")
+            name = tokens[0]
+            if name not in coeffs_negative:  #Skip for case where perms contain unfiltered peaks
+                continue
+            if count_trials[name] == max_trials:
+                extraFile.write(line)
+                continue
+            if (tokens[column2] != "0" and (tokens[column2][0] == "-") != coeffs_negative[name]) or (tokens[column2] == "0" and parsed_pvals[name][0] != 0):
+                continue #Reject
+            pval = tokens[column]
+            count_trials[name] = 1 + count_trials[name]
+            if leq(parseNum(pval), parsed_pvals[name]):
+                lower_trials[name] = lower_trials[name] + 1
+        
+
+outFile = open(sys.argv[3+nDir], "w")
+outFile.write(new_header)
+for i in range(len(names)):
+    name = names[i]
+    outFile.write(",".join([name, pvals[i], coeffs[i]]))
+    trials = count_trials[name]
+    missing_trials = 10 ** max(3, ceil(log10(trials))) - trials
+    low_trials = lower_trials[name]
+    outFile.write("," + str(lower_trials[name] / count_trials[name]) + "," + str(trials) + "," + str(missing_trials) + "," + str(low_trials) + "\n")
+outFile.close()
+extraFile.close()
---
+++ ocr_phylolm_conditional.r
@@ -0,0 +1,143 @@
+#Script to preform phylolm analysis on OCR activity predictions. This script is for command-line use.
+#Daniel Schaffer, Pfenning Lab
+#Takes several required positional arguments, in order: 
+#    tree file (Newick), 
+#    OCR activity predictions matrix file (no header, first column is OCR names, tab-seperated), 
+#    file listing species corresponding to predictions matrix columns (one per line, in the format Genus species), 
+#    phenotype file (CSV, species names in a column called "species binomial" in the format genus_species)
+#    output file name "template" (see below), 
+#    I, output file number and inital line in predictions matrix (see below), 
+#    J, step size in predictions matrix (see below), 
+#    (changed) The name of a CSV with columns Enhancer and Missing_Trials
+#         that specifies how many permulations (K) to do for each OCR
+#    S, random seed to use, 
+#    column in phenotype file
+#E.g. if putput file "template" given is /path/to/foo.csv, output will be in (I,S as above) /path/to/foo_rI_S.csv
+#Template must end in .csv
+#Will apply phylolm to K permulations for OCRs on lines I, I+J, I+2J, ... until end of matrix is reached
+
+library(ape)     #Phylogenetic tree processing
+library(geiger)  #Brownian motion simulation
+library(stringr) #Species name manipulation
+library(phylolm) #Phylogeny-corrected correlation
+
+
+#Slightly modified from RERconverge package
+#'Generates a permulated continuous phenotype given an observed continuous phenotype and a phylogeny
+#' @param namedvec A named numeric vector with phenotype values for each speices
+#' @param treewithbranchlengths A rooted phylogenetic tree with the same species as namedvec and branch lengths representing average evolutionary rate.
+#' @param rm A precomputed rate matrix
+#' @return A vector with permulated phenotype values
+#' @export
+simpermvec=function (namedvec, treewithbranchlengths, rm=NULL) 
+{
+  if(is.null(rm)) {
+    rm = ratematrix(treewithbranchlengths, namedvec)
+  }
+  sims = sim.char(treewithbranchlengths, rm, nsim = 1)
+  nam = rownames(sims)
+  s = as.data.frame(sims)
+  simulatedvec = s[, 1]
+  names(simulatedvec) = nam
+  simsorted = sort(simulatedvec)
+  realsorted = sort(namedvec)
+  l = length(simsorted)
+  c = 1
+  while (c <= l) {
+    simsorted[c] = realsorted[c]
+    c = c + 1
+  }
+  simsorted
+}
+
+args = commandArgs()
+seed = as.integer(args[14])
+set.seed(seed)
+
+tree = read.tree(file = args[6]) #Change to read.nexus for a nexus-format tree
+
+#Read phenotype data
+traits = read.csv(file= args[9])
+trait.col = args[15]
+trait.all = traits[[trait.col]]
+valid = which(trait.all != "NA") 
+trait = trait.all[valid]
+species.lower = traits$species.binomial[valid]
+trait.species = str_to_title(species.lower)
+names(trait) = trait.species
+
+#Read activity predictions
+preds = read.csv(file = args[7], header = F, sep = "\t")
+names(preds)[1] = "OCR"
+te = read.csv(file = args[8], header=F)
+pred.species = gsub(" ", "_", te$V1)
+names(preds)[2:(length(pred.species)+1)] = pred.species
+
+common.species = intersect(intersect(pred.species, tree$tip.label), trait.species)
+te = which(trait.species %in% common.species)
+tree.common = keep.tip(tree, common.species)
+
+
+########
+
+#Setup
+row_init = as.integer(args[11])
+row_step = as.integer(args[12])
+enh_details = read.csv(file = args[13], header = T)
+enh_shuffles = enh_details$Missing_Trials
+names(enh_shuffles) = enh_details$Enhancer
+enh_coeffs = enh_details$Coeff
+names(enh_coeffs) = enh_details$Enhancer
+
+max_iter = (nrow(preds)-row_init) %/% row_step
+n = (max_iter + 1) * max(enh_shuffles)
+
+enh.names = character(n)
+p.vals = double(n)
+coeffs = double(n)
+index = 1
+
+#Iterate & run phylolm
+options(warn = -1) #suppress warning from phylolm
+ptm <- proc.time()
+for (i in 0:max_iter) {
+  e = row_init + i*row_step
+  name = as.character(preds[e, 1])
+  if (name %in% names(enh_shuffles)) {
+    num_shuffles = enh_shuffles[name]
+    orig_coeff_sign = sign(enh_coeffs[name])  
+
+    cur.preds = preds[e, 2:(length(pred.species)+1)]
+    good.preds = cur.preds[which(cur.preds != -1)]
+    int.species = intersect(names(good.preds), common.species)
+    int.preds = good.preds[int.species]
+    int.trait = trait[int.species]
+    int.tree = keep.tip(tree.common, int.species)
+    int.tree.di = multi2di(int.tree)
+    rate.matrix=ratematrix(int.tree.di, int.trait)
+    int.trait.real = int.trait
+    names(int.trait.real) = int.species
+  
+    for (f in 1:num_shuffles){ 
+	  repeat {
+        int.trait = simpermvec(int.trait.real, int.tree.di, rm=rate.matrix)
+        dat <- data.frame(X = as.double(int.preds), Y = int.trait)
+        m <- phylolm(Y ~ X, data = dat, phy=int.tree.di, model = "BM")
+        m.coeff = summary(m)$coefficients
+	    if (sign(m.coeff[2]) == orig_coeff_sign) {
+	      enh.names[index] = name
+          p.vals[index] = m.coeff[8]
+          coeffs[index] = m.coeff[2]
+          index = index + 1
+		  break
+	    }
+      }
+    }
+  }	
+}
+proc.time() - ptm
+options(warn = 1)
+
+#Output
+dat = data.frame(OCR = enh.names[1:index-1], Pvalue = p.vals[1:index-1], Coeff = coeffs[1:index-1])
+write.csv(dat, sub(".csv", paste("_r", args[11], "_s", args[14], ".csv", sep=""),  args[10]), row.names = FALSE)
