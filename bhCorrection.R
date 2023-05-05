c314df8caaa7055bbea3589505ea72146f551526
imk1 <imk@alum.mit.edu>
2023-05-05 14:51:43 -0400

    I added scripts from Tianyu Liu for updating the Missing_Trials column for additional permulations runs and running the Benjamini-Hochberg procedure on permulations outputs.

---
+++ bhCorrection.R
@@ -0,0 +1,22 @@
+#Used to add a column 'bh' to adjust p value with bh
+#Usage1:
+# Rscript bhCorrection.R input.csv output.csv
+#Usage2:
+# Rscript bhCorrection.R input.csv output.csv Pvalue
+
+args <- commandArgs(trailingOnly = TRUE)
+input <- args[1]
+output <- args[2]
+# optional adjust column name
+if (length(args) >= 4) {
+  pName <- args[3]
+} else {
+  pName <- "Exp_Pvalue"
+}
+data <- read.csv(input)
+pval <- data[[pName]]
+bh = p.adjust(pval, method = "BH")
+data$bh=bh
+# sort table by adjusted P
+sorted <- data[order(data$bh),]
+write.table(sorted,output,sep=",",row.names = FALSE,quote = FALSE)
---
+++ permulationList.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import argparse
+
+'''
+Used to generate peak list and filter prediction matrix for permulation 
+Usage:
+
+python permulationList.py -i longevity_perm_10k_computedP.csv -t 0.0005 -p 900000 \
+-m $preds -o perm_1m_tmp
+
+'''
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i', '--input', required=True, help="csv file with Exp_Pvalue column and a column with peak name")
+	parser.add_argument('-t', '--threshold', required=True, help="threshold used to filter peaks by p-values")
+	parser.add_argument('-p', '--permulation', required=False, help="value append to Missing_Trials column")
+	parser.add_argument('-m', '--matrix', required=False, help="prediction matrix if wants to get filered, expect tsv, expect NO header")
+	parser.add_argument('-n', '--colName', required=False, help="column name for p value")
+	parser.add_argument('-o', '--output', required=True, help="output file name, e.g. perm10k")
+	
+	args = parser.parse_args()
+	df = pd.read_csv(args.input)
+	if args.colName is not None:
+		df = df.loc[df[args.colName] <= float(args.threshold)]
+	else:
+		df = df.loc[df['Exp_Pvalue'] <= float(args.threshold)] 
+
+	if args.permulation is not None:
+		df['Missing_Trials'] = args.permulation
+
+	df.to_csv(args.output+"_peakList.csv", index=False)
+	print("peak list output done")
+
+	if args.matrix is not None:
+		mat = pd.read_table(args.matrix, header=None)
+		# Important: assumes peak name is in the first column
+		filterMat = mat[mat.iloc[:, 0].isin(df.iloc[:, 0])]
+		filterMat.to_csv(args.output+"_predictionMatrix.tsv", sep='\t', index=False, header=False)
+		print("matrix filtering done")
