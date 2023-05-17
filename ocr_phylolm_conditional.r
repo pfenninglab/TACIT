61726978645add782e87fb039f12d321a22c5a30
imk1 <imk@alum.mit.edu>
2023-05-17 17:16:52 -0400

    I started modifying ocr_phylolm_conditional.r to account for multiple traits.  I am not sure if everything works yet.

--- ocr_phylolm_conditional.r
+++ ocr_phylolm_conditional.r
@@ -12,6 +12,7 @@
 #         that specifies how many permulations (K) to do for each OCR
 #    S, random seed to use, 
 #    column in phenotype file
+#    additional columns in phenotype file used in phylolm (can be multiple, eliminate if considering only 1 phenotype)
 #E.g. if putput file "template" given is /path/to/foo.csv, output will be in (I,S as above) /path/to/foo_rI_S.csv
 #Template must end in .csv
 #Will apply phylolm to K permulations for OCRs on lines I, I+J, I+2J, ... until end of matrix is reached
@@ -58,13 +59,40 @@ tree = read.tree(file = args[6]) #Change to read.nexus for a nexus-format tree
 
 #Read phenotype data
 traits = read.csv(file= args[9])
-trait.col = args[15]
-trait.all = traits[[trait.col]]
-valid = which(trait.all != "NA") 
-trait = trait.all[valid]
+trait.forShuf = args[15]
+trait.col = args[15:length(args)]
+trait.all = traits[trait.col]
+if (length(args) == 15) {
+        # Convert trait.col into an array
+        trait.all = as.matrix(trait.all)
+}
+trait.allForShuf = traits[[trait.forShuf]]
+valid = c()
+for (i in 1:nrow(trait.all)) {
+  # Iterate through the rows of the matrix and find those with no NAs
+  NAPresent = FALSE
+  for (j in 1:ncol(trait.all)) {
+    # Iterate through the columns of the matrix and check if each entry is an NA
+    if (is.na(trait.all[i,j])) {
+      # The entry is an NA
+      NAPresent = TRUE
+      break
+    }
+  }
+  if (NAPresent == FALSE) {
+    # No NAs in current row
+    valid = c(valid, i)
+  }
+}
+trait = as.matrix(trait.all[valid,])
+if (length(valid) == 0) {
+  # No rows with values from all species
+  print("Warning: No species with phenotype annotations for all phenotypes.")
+}
+traitForShuf = trait.allForShuf[valid]
 species.lower = traits$species.binomial[valid]
 trait.species = str_to_title(species.lower)
-names(trait) = trait.species
+row.names(trait) = trait.species
 
 #Read activity predictions
 preds = read.csv(file = args[7], header = F, sep = "\t")
@@ -89,7 +117,7 @@ row_step = as.integer(args[12])
 enh_details = read.csv(file = args[13], header = T)
 enh_shuffles = enh_details$Missing_Trials
 names(enh_shuffles) = enh_details$OCR
-enh_coeffs = enh_details$Coeff
+enh_coeffs = enh_details$Coeff[,1]
 names(enh_coeffs) = enh_details$OCR
 
 max_iter = (nrow(preds)-row_init) %/% row_step
@@ -97,7 +125,7 @@ n = (max_iter + 1) * max(enh_shuffles)
 
 enh.names = character(n)
 p.vals = double(n)
-coeffs = double(n)
+coeffs = matrix(nrow=n, ncol=length(args) - 14)
 index = 1
 
 #Iterate & run phylolm
@@ -114,26 +142,39 @@ for (i in 0:max_iter) {
     good.preds = cur.preds[which(cur.preds != -1)]
     int.species = intersect(names(good.preds), common.species)
     int.preds = good.preds[int.species]
-    int.trait = trait[int.species]
+    int.trait = as.matrix(trait[int.species, ])
+    int.traitForShuf = traitForShuf[int.species]
     int.tree = keep.tip(tree.common, int.species)
     int.tree.di = multi2di(int.tree)
     rate.matrix=ratematrix(int.tree.di, int.trait)
     int.trait.real = int.trait
     names(int.trait.real) = int.species
+    if (length(args) > 15) {
+      # Other traits should be used as additional covariates
+      for (j in 2:ncol(int.trait)) {
+        # Add the other traits as covariates
+        int.preds = cbind(as.double(int.preds), int.trait[,j])
+      }
+    } else {
+      # Convert int.preds into a double
+      int.preds = as.double(int.preds)
+    }
   
     for (f in 1:num_shuffles){ 
-	  repeat {
-        int.trait = simpermvec(int.trait.real, int.tree.di, rm=rate.matrix)
-        dat <- data.frame(X = as.double(int.preds), Y = int.trait)
+      repeat {
+        int.trait[,1] = simpermvec(int.trait.real, int.tree.di, rm=rate.matrix)
+      	X = int.preds
+      	Y = int.trait[,1]
+      	dat <- data.frame(X = X, Y = Y)
         m <- phylolm(Y ~ X, data = dat, phy=int.tree.di, model = "BM")
         m.coeff = summary(m)$coefficients
-	    if (sign(m.coeff[2]) == orig_coeff_sign) {
-	      enh.names[index] = name
-          p.vals[index] = m.coeff[8]
-          coeffs[index] = m.coeff[2]
+	if (sign(m.coeff[2]) == orig_coeff_sign) {
+	  enh.names[index] = name
+          p.vals[index] = m.coeff[8 + 3*(length(args) - 15)]
+          coeffs[index,] = m.coeff[2:(length(args)-13)]
           index = index + 1
-		  break
-	    }
+	  break
+	}
       }
     }
   }	
@@ -142,5 +183,9 @@ proc.time() - ptm
 options(warn = 1)
 
 #Output
-dat = data.frame(OCR = enh.names[1:index-1], Pvalue = p.vals[1:index-1], Coeff = coeffs[1:index-1])
+dat = data.frame(OCR = enh.names[1:index-1], Pvalue = p.vals[1:index-1])
+for (i in 1:ncol(int.trait)) {
+  # Iterate through the additional coefficients and add them to the data frame
+  dat = cbind(dat, Coeff = coeffs[1:index-1,i])
+}
 write.csv(dat, sub(".csv", paste("_r", args[11], "_s", args[14], ".csv", sep=""),  args[10]), row.names = FALSE)
